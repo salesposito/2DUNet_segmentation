20e08539175f9dfac32eaa77670009a0b1265958
salesposito <salvatore.esp95@gmail.com>
2021-10-19 15:37:25 +0100

    attention_unet

---
+++ lung_segmentation.py
@@ -0,0 +1,165 @@
+import numpy as np
+import dicom
+import os
+import pydicom
+import matplotlib.pyplot as plt
+from glob import glob
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import scipy.ndimage
+from skimage import morphology
+from skimage import measure
+from skimage.transform import resize
+from sklearn.cluster import KMeans
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
+
+
+output_path = working_path = "/Users/salvatoreesposito/Downloads/"
+# g = glob(data_path + '/*.dcm')
+# # print ("Total of %d DICOM images.\nFirst 5 filenames:" % len(g))
+# # print (g[:5])
+
+def load_scan():
+    data_path = "/Users/salvatoreesposito/Downloads/datasets/manifest-1557326747206/LCTSC/LCTSC-Test-S1-102/11-04-2003-RTRCCTTHORAX8FHigh Adult-20444/0.000000-CT114545RespCT  3.0  B30f  50 Ex-81163"
+    slices=[pydicom.dcmread(data_path + '/'+s)for s in os.listdir(data_path)]
+    slices.sort(key = lambda x: int(x.InstanceNumber))
+    try:
+     slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+        
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16)
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+    
+    # Convert to Hounsfield units (HU)
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+    
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+        
+    image += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+
+id = 0
+patient=load_scan()
+imgs = get_pixels_hu(patient)
+np.save(output_path + "fullimages_%d.npy" % (id), imgs)
+
+
+
+def create_histogram():
+    file_used=output_path+"fullimages_%d.npy" % id
+    imgs_to_process = np.load(file_used).astype(np.float64) 
+    plt.hist(imgs_to_process.flatten(), bins=50, color='c')
+    plt.xlabel("Hounsfield Units (HU)")
+    plt.ylabel("Frequency")
+    plt.show()
+
+id=0
+imgs_to_process = np.load(output_path+'fullimages_{}.npy'.format(id))
+
+def sample_stack(stack, rows=6, cols=6, start_with=10, show_every=3):
+    fig,ax = plt.subplots(rows,cols,figsize=[12,12])
+    for i in range(rows*cols):
+        ind = start_with + i*show_every
+        ax[int(i/rows),int(i % rows)].set_title('slice %d' % ind)
+        ax[int(i/rows),int(i % rows)].imshow(stack[ind],cmap='gray')
+        ax[int(i/rows),int(i % rows)].axis('off')
+    plt.show()
+
+# sample_stack(imgs_to_process)
+
+print ("Slice Thickness: %f" % patient[0].SliceThickness)
+print ("Pixel Spacing (row, col): (%f, %f) " % (patient[0].PixelSpacing[0], patient[0].PixelSpacing[1]))
+
+id = 0
+imgs_to_process = np.load(output_path+'fullimages_{}.npy'.format(id))
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = map(float, ([scan[0].SliceThickness] + list(scan[0].PixelSpacing)))
+    spacing = np.array(list(spacing))
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+    
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
+    
+    return image, new_spacing
+
+print ("Shape before resampling\t", imgs_to_process.shape)
+imgs_after_resamp, spacing = resample(imgs_to_process, patient, [1,1,1])
+print ("Shape after resampling\t", imgs_after_resamp.shape)
+
+
+def make_mesh(image, threshold=-300, step_size=1):
+
+    print ("Transposing surface")
+    p = image.transpose(2,1,0)
+    
+    print ("Calculating surface")
+    verts, faces, norm, val = measure.marching_cubes(p, threshold, step_size=step_size, allow_degenerate=True) 
+    return verts, faces
+
+def plotly_3d(verts, faces):
+    x,y,z = zip(*verts) 
+    
+    print ("Drawing")
+    
+    # Make the colormap single color since the axes are positional not intensity. 
+#    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+    colormap=['rgb(236, 236, 212)','rgb(236, 236, 212)']
+    
+    fig = FF.create_trisurf(x=x,
+                        y=y, 
+                        z=z, 
+                        plot_edges=False,
+                        colormap=colormap,
+                        simplices=faces,
+                        backgroundcolor='rgb(64, 64, 64)',
+                        title="Interactive Visualization")
+    iplot(fig)
+
+def plt_3d(verts, faces):
+    print ("Drawing")
+    x,y,z = zip(*verts) 
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+    face_color = [1, 1, 0.9]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0, max(y))
+    ax.set_zlim(0, max(z))
+    ax.set_facecolor((0.7, 0.7, 0.7))
+    plt.show()
+
+v, f = make_mesh(imgs_after_resamp, 350)
+plt_3d(v, f)
+
+
+v, f = make_mesh(imgs_after_resamp, 350, 2)
+plotly_3d(v, f)
+
